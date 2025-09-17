83cfb5bfa8604fe4d02efed928ca30b2a1479387
Mike Treit <mtreit@hotmail.com>
2025-09-17 10:40:40 -0700

    Sample app using net472

---
+++ SimpleConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+
+namespace SimpleConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var installDir = HttpRuntime.ClrInstallDirectory;
+            Console.WriteLine($"CLR Install Directory: {installDir}");
+        }
+    }
+}
